7d08a5e805dafcc13b2c102404e8c7fc2148ef43
Pablo Diosquez <pablodiosquez24@gmail.com>
2025-02-16 15:05:22 -0300

    Agregar clase Nodo

---
+++ iterator/src/main/java/nodo/Nodo.java
@@ -0,0 +1,23 @@
+package nodo;
+
+public class Nodo<T> {
+    private T dato;
+    private Nodo<T> prox;
+    
+    public Nodo(T dato, Nodo<T> prox){
+        this.dato = dato;
+        this.prox = prox;
+    }
+
+    public Nodo(T dato){
+        this(dato, null);
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public Nodo<T> getProx() {
+        return prox;
+    }
+}
